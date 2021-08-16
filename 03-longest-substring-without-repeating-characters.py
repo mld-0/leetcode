e0b9f3cb1c621b6b7f70caf2d30ac627df2ccd94
Matthew L Davis <mld.0@protonmail.com>
2021-08-17 03:02:08 +1000

    alt solution, sliding window
    Reduces search from O(n**2) to O(n). Result beats 20% submissions.
    Memory usage comperable to almost all other submissions.

--- 03-longest-substring-without-repeating-characters.py
+++ 03-longest-substring-without-repeating-characters.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+import functools
 
 class Solution:
 
@@ -7,8 +9,29 @@ class Solution:
             return False
         return True
 
-    #   Runtime (leetcode) 4732ms (beats 5%)
+
+    # Sliding window algorithm: 
+    #   Results:
+    #       runtime: 288ms (beats 20%)
+    #       memory: run-dependent
+    # LINK: https://levelup.gitconnected.com/an-introduction-to-sliding-window-algorithms-5533c4fe1cc7
     def lengthOfLongestSubstring(self, s: str) -> int:
+        longest = 0
+        left, right = 0, 0
+        window_counter = defaultdict(int)
+        # Iterate right bound of window across s. left bounds is only iterated if duplicate char encountered
+        for right in range(0, len(s)):
+            window_counter[s[right]] += 1
+            # If the current window contains a duplicate, increment 'left'
+            if any([v > 1 for v in window_counter.values()]):
+                window_counter[s[left]] -= 1
+                left += 1
+            longest = max(longest, right - left + 1)
+        return longest
+
+
+    #   Runtime (leetcode) 4732ms (beats 5%)
+    def lengthOfLongestSubstring_Brute(self, s: str) -> int:
         longest = ""
         for i, val_i in enumerate(s):
             for j in range(i, len(s)+1):
@@ -17,16 +40,18 @@ class Solution:
                         longest = s[i:j]
                 else:
                     break
-
         return len(longest)
 
 val_str = "abcabcbb"
 s = Solution()
+#result = s.lengthOfLongestSubstring_Brute(val_str)
 result = s.lengthOfLongestSubstring(val_str)
 print(result)
 
 val_str = " "
 s = Solution()
+#result = s.lengthOfLongestSubstring_Brute(val_str)
 result = s.lengthOfLongestSubstring(val_str)
 print(result)
+
 
