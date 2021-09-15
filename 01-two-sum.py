f3c12c617e9831d6bfc2e829ec4dd3ea20adf3f8
Matthew L Davis <mld.0@protonmail.com>
2021-09-15 20:18:42 +1000

    fix print, return tuple instead of list

--- 01-two-sum.py
+++ 01-two-sum.py
@@ -13,23 +13,20 @@ class Solution:
             delta = target - nums[index]
 
             if delta in num_to_index.keys():
-                return [ num_to_index[delta], index ]
+                return ( num_to_index[delta], index )
 
             num_to_index[nums[index]] = index
 
 
 nums_targets_list = [ ([2,7,11,15], 9), ([3,2,4], 6), ([3,3], 6) ]
-check_list = [ [0,1], [1,2], [0,1] ]
+check_list = [ (0,1), (1,2), (0,1) ]
 
 s = Solution()
 
 for (nums, target), check in zip(nums_targets_list, check_list):
     result = s.twoSum(nums, target)
-    print("nums=(%s), target=(%s)" % (nums, target))
-    print("result=(%s)" % result)
+    print("nums=(%s), target=(%s)" % (str(nums), str(target)))
+    print("result=(%s)" % str(result))
     print()
     assert( result == check )
-
-
-
 
