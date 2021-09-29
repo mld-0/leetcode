79406d1828074aa95990269b7c018097d9dceb7a
Matthew L Davis <mld.0@protonmail.com>
2021-09-29 22:11:30 +1000

    custom memorization decorator for class method

--- 70-climbing-stairs.py
+++ 70-climbing-stairs.py
@@ -1,5 +1,16 @@
 import functools
 
+#   On the use of a memorization wrapper on a class method (see below)
+#   LINK: https://stackoverflow.com/questions/51138226/how-to-apply-a-memorize-decorator-to-an-instance-method
+def memorize_wraper(f):
+    cache = {}
+    @functools.wraps(f)
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = f(*args)
+        return cache[args]
+    return wrapper
+
 class Solution:
 
     memorize = { 0: 0, 1: 1, 2: 2, }
@@ -7,7 +18,8 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         #return self.climbStairs_Recursive(n)
         #return self.climbStairs_RecursiveMemorize(n)
-        return self.climbStairs_Dynamic(n)
+        return self.climbStairs_RecursiveDecorator(n)
+        #return self.climbStairs_Dynamic(n)
 
 
     #   runtime: TLE
@@ -23,6 +35,13 @@ class Solution:
         result = self.climbStairs_RecursiveMemorize(n-1) + self.climbStairs_RecursiveMemorize(n-2)
         self.memorize[n] = result
         return result
+
+    #   runtime: beats 83%
+    @memorize_wraper
+    def climbStairs_RecursiveDecorator(self, n: int) -> int:
+        if n < 3:
+            return n
+        return self.climbStairs_RecursiveDecorator(n-1) + self.climbStairs_RecursiveDecorator(n-2)
 
     #   runtime: beats 99%
     @functools.lru_cache()
