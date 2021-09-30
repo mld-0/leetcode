7880ad5d92c623d479ada9e19aa84b35562464a3
Matthew L Davis <mld.0@protonmail.com>
2021-10-01 02:48:47 +1000

    cleanup, diy-memoization, top-down/bottom-up
    Top-down describes the recursive-memoization approach, bottom-up
    describes the iterative-table-filling approach.
    TODO is a neater version of tracing path for recursive solution, and
    tracing path (using table?) for iterative solution.

--- 931-min-falling-path-sum.py
+++ 931-min-falling-path-sum.py
@@ -7,37 +7,103 @@ import functools
 from typing import List
 #   {{{2
 
+#   TODO: 2021-09-30T21:14:12AEST _leetcode, 931-min-falling-path-sum, trace path of solution (for recursive and DP solutions)
+#   Ongoing: 2021-10-01T02:18:07AEST _leetcode, 931-min-falling-path-sum, why lru_cache recursive-memoized solution is TLE yet DIY dict-based memorized solution beats 28%?
+#   TODO: 2021-10-01T02:20:29AEST _leetcode, 931-min-falling-path-sum, (less horrific) re-write of 'Path' version of recursive solution
+
 class Solution:
 
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        #return self.minFallingPathSum_Recursive(matrix)
         #return self.minFallingPathSum_Path_Recursive(matrix)
-        return self.minFallingPathSum_DP_BottomUp(matrix)
+        return self.minFallingPathSum_DP(matrix)
+        #return self.minFallingPathSum_RecursiveMemoized(matrix)
+
+
+    #   A 'top-down' DP approach is recursion-memorization
+    #   runtime: beats 28%
+    def minFallingPathSum_RecursiveMemoized(self, matrix: List[List[int]]) -> int:
+        self.memoized = {}
+
+        def min_path(row, col):
+            """Calculate the min-falling-path starting from a given cell in the first row, with a dict used to memoize results"""
+            if (row,col) in self.memoized:
+                return self.memoized[(row,col)]
+
+            path = matrix[row][col]
+            trials = [ math.inf ] * 3
+
+            if row < len(matrix) - 1:
+                if col-1 >= 0:
+                    trials[0] = min_path(row+1, col-1)
+                if col+1 < len(matrix[row]):
+                    trials[2] = min_path(row+1, col+1)
+                trials[1] = min_path(row+1, col)
+
+                #   index of which gives next 'step' in path for current call stack
+                path += min(trials)
+
+            self.memoized[(row,col)] = path
+            return path
+
+        #   get the min_path() for each cell in first row as 'results'
+        results = []
+        for col in range(len(matrix[0])):
+            results.append( min_path(0, col) )
+        print(results)
+
+        #   index of which gives starting cell, complete path is given by adding the starting cell to the 'steps' in the call stack corresponding to the starting cell
+
+        #   solution given by:
+        return min(results)
+
+
+    #   A 'bottom-up' DP approach is table-filling
+    #   runtime: beats 87%
+    def minFallingPathSum_DP(self, matrix: List[List[int]]) -> int:
+        """Calculate the min-falling-path, iteratively with table, and rule defining cells in terms of previous row"""
+        #   table 'grid' is initalized with values of matrix
+        grid = matrix[:]
+
+        #   skipping first row, for each cell in subsiquent rows/columns, add whichever of the cells above and adjacent have the smallest value
+        for row in range(1, len(matrix)):
+            for col in range(len(matrix[row])):
+                trials = [ math.inf ] * 3
+
+                if col-1 >= 0:
+                    trials[0] = grid[row-1][col-1]
+                if col+1 < len(matrix[row]):
+                    trials[2] = grid[row-1][col+1]
+                trials[1] = grid[row-1][col]
+
+                grid[row][col] += min(trials)
+
+        #   (can path be determined from examining 'grid'?)
+
+        #   solution given by:
+        return min(grid[-1])
+     
 
 
     #   runtime: TLE
     def minFallingPathSum_Recursive(self, matrix: List[List[int]]) -> int:
+        #   {{{
         @functools.lru_cache()
         def min_path(row, col):
             path = matrix[row][col]
             offset_deltas = [ -1, 0, 1 ]
-
             trials = [ math.inf ]
             if row < len(matrix) - 1:
                 for delta_col in offset_deltas:
                     if col+delta_col >= 0 and col+delta_col < len(matrix[0]):
                         trials.append( min_path(row+1, col+delta_col ) )
                 path += min(trials)
-
             return path
-        
         results = []
         for col in range(len(matrix[0])):
             results.append( min_path(0, col) )
         print(results)
-
         return min(results)
-
+        #   }}}
 
     #   runtime: TLE
     def minFallingPathSum_Path_Recursive(self, matrix: List[List[int]]) -> int:
@@ -86,35 +152,6 @@ class Solution:
         print("values_traveled=(%s)" % str(values_traveled))
         return results[results_min_index]
     #   }}}
-
-
-    #   runtime: beats 54%
-    def minFallingPathSum_DP_BottomUp(self, matrix: List[List[int]]) -> int:
-        grid = matrix[:]
-        for row in range(1, len(matrix)):
-            for col in range(len(matrix[row])):
-                trials = [ math.inf ] * 3
-
-                if row-1 >= 0 and col-1 >= 0:
-                    trials[0] = grid[row-1][col-1]
-                if row-1 >= 0:
-                    trials[1] = grid[row-1][col]
-                if row-1 >= 0 and col+1 < len(matrix[row]):
-                    trials[2] = grid[row-1][col+1]
-
-                grid[row][col] += min(trials)
-
-        return min(grid[-1])
-        
-
-    #   TODO: _leetcode, 931-min-falling-path-sum, dynamic programming 'top down' solution
-    def minFallingPathSum_DP_TopDown(self, matrix: List[List[int]]) -> int:
-        raise NotImplementedError()
-
-
-    #   TODO: 2021-09-30T21:14:12AEST _leetcode, 931-min-falling-path-sum, trace path of solution (for recursive and DP solutions)
-    def minFallingPathSum_Path_DP(self, matrix: List[List[int]]) -> int:
-        raise NotImplementedError()
 
 
 s = Solution()
