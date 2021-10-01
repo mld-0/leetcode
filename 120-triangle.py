4fc9f84afdd59b275075811f6dfc2e461a1e8caa
Matthew L Davis <mld.0@protonmail.com>
2021-10-01 21:19:40 +1000

    dynamic-programming top-down/bottom-up solutions
    Top Down (recursive-memoization) and Bottom Up (iterative-table)
    solutions. Problem 'Triangle' is a special case of
    '931-min-falling-path-sum'.

--- 120-triangle.py
+++ 120-triangle.py
@@ -1,14 +1,65 @@
+#   VIM SETTINGS: {{{3
+#   vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
+#   vim: set foldlevel=2 foldcolumn=2:
+#   }}}1
 import functools
+import math
 from typing import List
+#   {{{2
 
 class Solution:
 
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         """Find minimum path sum from top-to-bottom, traveling from i on a given row to either i or i+1 on the next row"""
-        return self.minimumTotal_Recursive(triangle)
+        return self.minimumTotal_TopDown(triangle)
+        #return self.minimumTotal_BottomUp(triangle)
+
+    #   runtime: beats 5%
+    def minimumTotal_TopDown(self, triangle: List[List[int]]) -> int:
+        self.memo = {}
+
+        def min_path(row, col):
+            if (row,col) in self.memo:
+                return self.memo[(row,col)]
+
+            #   last row, no recursive calls to make
+            if row == len(triangle) - 1:
+                self.memo[(row,col)] = triangle[row][col]
+                return triangle[row][col]
+
+            #   cost for (row, col) is given by min of two cells below it
+            trials = [ min_path(row+1, col), min_path(row+1, col+1) ]
+            path = triangle[row][col] + min(trials)
+
+            self.memo[(row,col)] = path
+            return path
+
+        result = min_path(0,0)
+        return result
+
+
+    #   runtime: beats 33%
+    def minimumTotal_BottomUp(self, triangle: List[List[int]]) -> int:
+        grid = triangle[:]
+
+        for row in range(1, len(grid)):
+            for col in range(len(grid[row])):
+
+                trials = [ math.inf ] * 2
+                if col-1 >= 0:
+                    trials[0] = grid[row-1][col-1]
+                if col < len(grid[row-1]):
+                    trials[1] = grid[row-1][col]
+
+                grid[row][col] += min(trials)
+
+        print(grid)
+        return min(grid[-1])
+
 
     #   runtime: beats 15%
     def minimumTotal_Recursive(self, triangle: List[List[int]]) -> int:
+        #   {{{
         @functools.lru_cache()
         def min_path(row, col):
             path = triangle[row][col]
@@ -23,9 +74,11 @@ class Solution:
             return path
         result = min_path(0,0)
         return result
+        #   }}}
 
     #   runtime: beats 5%
     def minimumTotal_WithPath_Recursive(self, triangle: List[List[int]]) -> int:
+        #   {{{
         @functools.lru_cache()
         def min_path(row, col):
             steps = []
@@ -44,21 +97,14 @@ class Solution:
         steps = steps + [(0,0)] 
         print(steps)
         return result
+        #   }}}
 
-
-    #   TODO: 2021-09-30T20:17:16AEST _leetcode, 120-triangle, Dynamic Programming, BottomUp and TopDown
-    def minimumTotal_DP_BottomUp(self, triangle: List[List[int]]) -> int:
-        raise NotImplementedError()
-
-
-    def minimumTotal_DP_TopDown(self, triangle: List[List[int]]) -> int:
-        raise NotImplementedError()
 
 
 s = Solution()
 
-input_values = [ [[2],[3,4],[6,5,7],[4,1,8,3]], [[-10]], ]
-input_checks = [ 11, -10, ]
+input_values = [ [[2],[3,4],[6,5,7],[4,1,8,3]], [[-10]], [[-1],[3,2],[-3,1,-1]], ]
+input_checks = [ 11, -10, -1, ]
 
 for triangle, check in zip(input_values, input_checks):
     result = s.minimumTotal(triangle)
