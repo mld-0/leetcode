3aee0a89768d1b53a855a2b1b5e2ae26139bbef1
Matthew L Davis <mld.0@protonmail.com>
2021-10-12 20:00:53 +1100

    cleanup

--- 11-container-with-most-water.py
+++ 11-container-with-most-water.py
@@ -1,41 +1,47 @@
+#   VIM SETTINGS: {{{3
+#   vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
+#   vim: set foldlevel=2 foldcolumn=2:
+#   }}}1
+#   {{{2
+from typing import List
 
 class Solution:
 
-    def calcArea(self, height: list[int], start: int, end: int) -> int:
-        return min(height[end], height[start]) * abs(end - start)
+    def maxArea(self, height: List[int]) -> int:
+        return self.maxArea_TwoPointers(height)
 
-    #   Result:
-    #       runtime: 632ms (beats 96%)
-    def maxArea_Second(self, height: list[int]) -> int:
+    
+    #   runtime: beats 99%
+    def maxArea_TwoPointers(self, height: List[int]) -> int:
         V = 0
-        start = i = 0
-        end = j = len(height)-1
-        
-        while (i < j):
-            #   Evaluate h/V 
-            h = min(height[i], height[j])
-            trial_V = h * (j - i)
-            if (trial_V > V):
+        endpoints = [None, None]
+
+        l = 0
+        r = len(height) - 1
+
+        while l < r:
+            #   evaluate trial height/volume 
+            trial_H = min(height[l], height[r])
+            trial_V = trial_H * (r - l)
+            if trial_V > V:
                 V = trial_V
-                start = i
-                end = j
-            
-            #   Increment i until height[i] > h (or out of bounds)
-            while (h >= height[i] and i < j):
-                i += 1
-            
-            #   Decrement j until height[j] > h (or out of bounds)
-            while (h >= height[j] and i < j):
-                j -= 1
-            
-        return V
+                endpoints = [l, r]
 
-    #   Result:
-    #       runtime: Time Limit Exceded
-    def maxArea_BruteForce(self, height: list[int]) -> int:
+            #   Increment 'l' until height[l] > trial_H (or out of bounds)
+            while (trial_H >= height[l] and l < r):
+                l += 1
+
+            #   Decrement 'r' until height[r] > trial_H (or out of bounds)
+            while (trial_H >= height[r] and l < r):
+                r -= 1
+
+        return V
+        
+
+    #   runtime: TLE
+    def maxArea_BruteForce(self, height: List[int]) -> int:
         V = 0
-        start = 0
-        end = len(height)-1
+        endpoints = [0, len(height)-1]
 
         for i in range(0, len(height)-1):
             for j in range(1, len(height)):
@@ -43,58 +49,20 @@ class Solution:
                 trial_V = h * (j - i)
                 if trial_V > V:
                     V = trial_V
-                    start = i
-                    end = j
+                    endpoints = [i, j]
 
         return V
 
 
-    #   Wrong
-    def maxArea_First(self, height: list[int]) -> int:
-        start = 0 
-        end = len(height)-1
-        #   while there is a line in height[start+1, end] > min(height[start], height[end])
-        while ( any([ height[x] > min(height[start], height[end]) for x in range(start+1, end) ]) ):
-            #   What is the tallest line between [start+1, end)
-            index_max = start+1
-            #if height[end] < height[start]:
-            #    index_max = end
-            for i in range(start+1, end):
-                if height[i] > height[index_max]:
-                    index_max = i
-            #   If swapping whichever of start/end has the smaller height with index_max produces a larger area, do so and continue, otherwise break (and return area as per current start/end)
-            if height[end] > height[start]:
-                if (self.calcArea(height, index_max, end) > self.calcArea(height, start, end)):
-                    start = index_max
-                else:
-                    break
-            else:
-                if (self.calcArea(height, start, index_max) > self.calcArea(height, start, end)):
-                    end = index_max
-                else:
-                    break
-
-        return self.calcArea(height, start, end)
-
-
-    def maxArea(self, height: list[int]) -> int:
-        return self.maxArea_BruteForce(height)
-
 s = Solution()
-
-values = [ 1,8,6,2,5,4,8,3,7 ]
-check = 49
-result = s.maxArea(values)
-print("result=(%s)" % result)
-assert(result == check)
-
 
 input_items = [ [1,8,6,2,5,4,8,3,7], [1,1], [4,3,2,1,4], [1,2,1], [1,2,4,3] ]
 check_items = [ 49, 1, 16, 2, 4 ]
 
-for loop_input, loop_check in zip(input_items, check_items):
-    print("loop_input=(%s)" % str(loop_input))
-    loop_result = s.maxArea(loop_input)
-    print("loop_result=(%s)" % loop_result)
-    assert(loop_result == loop_check)
+for heights, check in zip(input_items, check_items):
+    print("heights=(%s)" % str(heights))
+    result = s.maxArea(heights)
+    print("result=(%s)" % result)
+    assert result == check, "Check failed"
+    print()
 
