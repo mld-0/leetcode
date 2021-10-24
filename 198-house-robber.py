e8134c962531ac2e39c5d5455ec377c2e90a5d78
Matthew L Davis <mld.0@protonmail.com>
2021-10-24 20:29:44 +1100

    nested func for recursive-memorize, test all funcs
    Not having to use class variables makes for a neater solution.

--- 198-house-robber.py
+++ 198-house-robber.py
@@ -12,36 +12,45 @@ class Solution:
         return self.rob_DP_SpaceOptimised(nums)
 
 
+
     #   runtime: beats 97%
     def rob_RecursiveMemorize(self, nums: List[int], i: int=0) -> int:
         """Recursively decide whether to skip house i in 'nums', or to rob i and i+1"""
-        #   no more houses
-        if i >= len(nums):
-            return 0
+        maxRobbedAmount = dict()
+        decisions = dict()
 
-        #   used memorized solution
-        if i in self.maxRobbedAmount:
-            return self.maxRobbedAmount[i]
+        def rob_solve(nums, i):
+            #   no more houses
+            if i >= len(nums):
+                return 0
 
-        #   skip current house
-        trial_A = self.rob_RecursiveMemorize(nums, i+1)
+            #   used memorized solution
+            if i in maxRobbedAmount:
+                return maxRobbedAmount[i]
 
-        #   rob current house
-        trial_B = self.rob_RecursiveMemorize(nums, i+2) + nums[i]
+            #   skip current house
+            trial_A = rob_solve(nums, i+1)
 
-        ans = trial_A
-        if trial_A < trial_B:
-            ans = trial_B
+            #   rob current house
+            trial_B = rob_solve(nums, i+2) + nums[i]
 
-            #   we are robbing houses i, i+2, and skipping i+1
-            self.decisions[i] = 'rob'
-            if i+1 in self.decisions:
-                del self.decisions[i+1]
-            if i+2 < len(nums):
-                self.decisions[i+2] = 'rob'
+            ans = trial_A
+            if trial_A < trial_B:
+                ans = trial_B
 
-        self.maxRobbedAmount[i] = ans
-        return ans
+                #   store decision: we are robbing houses i, i+2, and skipping i+1
+                decisions[i] = 'rob'
+                if i+1 in decisions:
+                    del decisions[i+1]
+                if i+2 < len(nums):
+                    decisions[i+2] = 'rob'
+
+            maxRobbedAmount[i] = ans
+            return ans
+
+        result = rob_solve(nums, i)
+        print(decisions)
+        return result
 
 
     #   runtime: beats 37%
@@ -59,7 +68,7 @@ class Solution:
             if trial_A < trial_B:
                 maxRobbedAmount[i] = trial_B
 
-                #   we are robbing houses i, i+2, and skipping i+1
+                #   store decision: we are robbing houses i, i+2, and skipping i+1
                 decisions[i] = 'rob'
                 if i+1 in decisions:
                     del decisions[i+1]
@@ -107,13 +116,17 @@ class Solution:
 
 
 s = Solution()
+test_functions = [ s.rob_RecursiveMemorize, s.rob_DP, s.rob_DP_SpaceOptimised, ] 
 
 input_values = [ [1,2,3,1], [2,7,9,3,1], ]
 input_checks = [ 4, 12 ]
 
-for nums, check in zip(input_values, input_checks):
-    result = s.rob(nums)
-    print("result=(%s)" % str(result))
-    assert check == result, "Check failed"
+for test_func in test_functions:
+    print(test_func.__name__)
+    for nums, check in zip(input_values, input_checks):
+        print("nums=(%s)" % nums)
+        result = test_func(nums)
+        print("result=(%s)" % str(result))
+        assert check == result, "Check failed"
     print()
 
