4700af6f438817ecf3b08f428e1f5e0dd1ad9b39
Matthew L Davis <mld.0@protonmail.com>
2022-09-01 20:36:41 +1000

    three-pointers in-place solution(s), and ans
    Previous solutions used extra memory, or involved sorting.
    Whenever one needs to modify an array in-place, consider iterating
    backwards instead of forwards. First in-place solution we swapped values
    (which it turns out is unnecessary).

--- 88-merge-sorted-array.py
+++ 88-merge-sorted-array.py
@@ -1,3 +1,7 @@
+#   {{{3
+#   vim: set tabstop=4 modeline modelines=10:
+#   vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
+#   {{{2
 from typing import List
 
 class Solution:
@@ -9,7 +13,7 @@ class Solution:
         j = 0
         k = 0
         while i < m and j < n:
-            if (nums1[i] <= nums2[j]):
+            if nums1[i] <= nums2[j]:
                 result[k] = nums1[i]
                 i += 1
             else:
@@ -33,40 +37,74 @@ class Solution:
         nums1.sort()
     
 
-    #   Continue: 2022-08-28T21:39:35AEST leetcode, 88-merge-sorted-array, merge_inplace (two/three-pointers)
-    def merge_inplace(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        ...
-        #i = 0
-        #j = 0
-        #while i < m and j < n:
-        #    if nums1[i] <= nums2[j]:
-        #        pass
-        #    else:
-        #        nums1[m] = nums1[i]
-        #        nums1[i] = nums2[j]
-        #        j += 1
-        #        #i -= 1
-        #    print("nums1=(%s), i=(%s), nums2=(%s), j=(%s)" % (nums1, i, nums2, j))
-        #    i += 1
-        #print("nums1=(%s), i=(%s), nums2=(%s), j=(%s)" % (nums1, i, nums2, j))
-        #while j < n:
-        #    nums1[i] = nums2[j]
-        #    print("nums1=(%s), i=(%s), nums2=(%s), j=(%s)" % (nums1, i, nums2, j))
-        #    i += 1
-        #    j += 1
+    #   Whenever one needs to modify an array in-place, consider iterating backwards instead of forward
+    #   runtime: beats 99%
+    def merge_threePointers_inplace_i(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[i], nums1[k] = nums1[k], nums1[i]
+                i -= 1
+            else:
+                nums2[j], nums1[k] = nums1[k], nums2[j]
+                j -= 1
+            k -= 1
+        while i >= 0:
+            nums1[k], nums1[i] = nums1[i], nums1[k]
+            i -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k], nums2[j] = nums2[j], nums1[k]
+            j -= 1
+            k -= 1
+
+
+    #   runtime: beats 98%
+    def merge_threePointers_inplace_ii(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+
+
+    #   runtime: beats 96%
+    def merge_ans(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = m - 1
+        j = n - 1
+        for k in range(n+m-1, -1, -1):
+            if j < 0:
+                break
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
 
 
 s = Solution()
-test_functions = [ s.merge_threePointers, s.merge_using_sorting, ]
-#test_functions = [ s.merge_threePointers, s.merge_using_sorting, s.merge_inplace, ]
+test_functions = [ s.merge_threePointers, s.merge_using_sorting, s.merge_threePointers_inplace_i, s.merge_threePointers_inplace_ii, s.merge_ans, ]
 
-inputs = [ ([1,2,3,0,0,0], 3, [2,5,6], 3), ([1], 1, [], 0), ([0], 0, [1], 1), ([4,5,6,0,0,0], 3, [1,2,3], 3), ]
-validation = [ [1,2,2,3,5,6], [1], [1], [1,2,3,4,5,6], ]
-assert len(inputs) == len(validation)
+input_values = [ ([1,2,3,0,0,0], 3, [2,5,6], 3), ([1], 1, [], 0), ([0], 0, [1], 1), ([4,5,6,0,0,0], 3, [1,2,3], 3), ([1,2,3,4,5], 5, [], 0), ([1,2,4,5,6,0], 5, [3], 1), ]
+result_validation = [ [1,2,2,3,5,6], [1], [1], [1,2,3,4,5,6], [1,2,3,4,5], [1,2,3,4,5,6], ]
+assert len(input_values) == len(result_validation)
 
 for f in test_functions:
     print(f.__name__)
-    for (nums1, m, nums2, n), check in zip(inputs, validation):
+    for (nums1, m, nums2, n), check in zip(input_values, result_validation):
         nums1 = nums1[:]
         nums2 = nums2[:]
         print("nums1=(%s), m=(%s), nums2=(%s), n=(%s)" % (nums1, m, nums2, n))
