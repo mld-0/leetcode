c2ad54a7a07bf17c4cbf717d33fdb2cc8a0e7c25
Matthew L Davis <mld.0@protonmail.com>
2022-09-05 23:26:24 +1000

    29-divide-two-integers, first 2 solutions
    Integer division implementations that don't overflow, taken from ans.
    Iterative and iterative-powers-of-2 solutions. We convert input numbers
    to negative to avoid overflow (there are more negative ints than
    positive ints) (making even the first, trivial solution complicated).
    Continue: remaining solutions (adding powers-of-2 with/without bitshift
    and binary-long-division), and timing each function.

---
+++ 29-divide-two-integers.cpp
@@ -0,0 +1,165 @@
+//	{{{3
+//	vim: set tabstop=4 modeline modelines=10:
+//	vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
+//	{{{2
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <string>
+#include <assert.h>
+#include <limits.h>
+using namespace std;
+//	Continue: 2022-09-05T22:58:26AEST main(), time each outer loop
+//	Continue: 2022-09-05T23:14:08AEST remaining solutions, (AddPower2, BinaryLongDivision) (and whether Bitshift version of former is different enough to justify being a seperate example)
+
+//	Implement integer division without using the '*', '/', or '%' operators, and >32-bit types, avoiding overflow.
+//	Constraints: (dividend,divisor) = [-2**31, 2**31-1], divisor != 0
+
+//	Substitute operations:
+//		a * -1			-a
+//		a / 2**n		a >> n
+//		a * 2**n		a << n
+//		a += a			a *= 2 			a <<= 1
+
+//	Overflow is platform specific behaviour -> any solution in which overflow occurs is not portable.
+
+//	Note: 'abs(INT_MIN) > INT_MAX'
+//	There are more negative integers than positive integers. 
+
+int f_ref(int dividend, int divisor) {
+	return dividend / divisor;
+}
+
+//	Ongoing: 2022-09-05T22:32:51AEST can't handle {INT_MIN,INT_MIN} case -> that was the 'dividend - divisor <= 0' line (the solution is 'divisor >= dividend')
+int f_iterative(int dividend, int divisor) {
+	//	Overflow case:
+	if (dividend == INT_MAX && divisor == -1) {
+		return INT_MAX;
+	}
+	//	Converting inputs to negative allows coverage of the largest possible range of numbers (to avoid overflow).
+	//	We store the number of inputs that were previously negative to allow us to determine correct sign of result.
+	int count_negatives = 2;
+	if (dividend > 0) {
+		--count_negatives;
+		dividend = -dividend;
+	}
+	if (divisor > 0) {
+		--count_negatives;
+		divisor = -divisor;
+	}
+	//	How many times can we fit 'divisor' into 'dividend'
+	int result = 0;
+	while (divisor >= dividend) { 	// (dividend - divisor <= 0) 
+		dividend -= divisor;
+		--result;
+	}
+	//	Return result with correct sign
+	if (count_negatives != 1) {
+		return -result;
+	}
+	return result;
+}
+
+
+//	Can't handle negative divisor
+int f_simple_iterativePower2(int dividend, int divisor) {
+	int result = 0;
+	while (dividend >= divisor) {
+		int powerOf2 = 1;
+		int temp = divisor;
+		while (temp + temp < dividend) {
+			temp <<= 1;
+			powerOf2 <<= 1;
+		}
+		result += powerOf2;
+		dividend -= temp;
+	}
+	return result;
+}
+
+
+int f_iterativePower2(int dividend, int divisor) {
+	//	Overflow case:
+	if (dividend == INT_MAX && divisor == -1) {
+		return INT_MAX;
+	}
+	//	Converting inputs to negative allows coverage of the largest possible range of numbers (to avoid overflow).
+	//	We store the number of inputs that were previously negative to allow us to determine correct sign of result.
+	int count_negatives = 2;
+	if (dividend > 0) {
+		--count_negatives;
+		dividend = -dividend;
+	}
+	if (divisor > 0) {
+		--count_negatives;
+		divisor = -divisor;
+	}
+	//	How many times can we fit 'divisor' into 'dividend'
+	//	sum(n) for each 'temp = divisor * 2**n' we can fit into 'dividend'
+	int result = 0;
+	while (dividend <= divisor) {		//	(divisor >= dividend), (dividend - divisor <= 0) 
+		int powerOf2 = -1;
+		int temp = divisor;
+		while (temp + temp >= dividend && temp >= INT_MIN/2) {
+			temp <<= 1;
+			powerOf2 <<= 1;
+		}
+		result += powerOf2;
+		dividend -= temp;
+	}
+	//	Return result with correct sign
+	if (count_negatives != 1) {
+		return -result;
+	}
+	return result;
+}
+
+
+int f_iterativeAddPower2s(int dividend, int divisor) {
+	return 0;
+}
+
+
+int f_iterativeAddPower2sBitshift(int dividend, int divisor) {
+	return 0;
+}
+
+
+int f_BinaryLongDivision(int dividend, int divisor) {
+	return 0;
+}
+
+
+vector<function<int(int,int)>> test_functions = { f_ref, f_iterative, f_iterativePower2, };
+vector<string> test_functions_names = { "f_ref", "f_iterative", "f_iterativePower2", };
+
+vector<tuple<int,int>> input_values = { {10,3}, {7,-3}, {INT_MAX,INT_MAX}, {INT_MIN,INT_MIN}, };
+vector<int> input_checks = { 3, -2, 1, 1, };
+
+int main()
+{
+	assert(sizeof(int) == 4);
+	assert(INT_MAX == 2147483647);
+	assert(INT_MIN == -2147483648);
+	assert(input_values.size() == input_checks.size());
+	assert(test_functions.size() == test_functions_names.size());
+
+	for (int i = 0; i < test_functions.size(); ++i) {
+		auto f = test_functions[i];
+		auto f_name = test_functions_names[i];
+		cout << f_name << ":\n";
+		for (int j = 0; j < input_values.size(); ++j) {
+			int dividend = get<0>(input_values[j]);
+			int divisor = get<1>(input_values[j]); 
+			cout << "dividend=(" << dividend << "), divisor=(" << divisor << ")\n";
+			int check = input_checks[j];
+			int result = f(dividend, divisor);
+			cout << "result=(" << result << ")\n";
+			assert(result == check && "Check comparison failed");
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
+
