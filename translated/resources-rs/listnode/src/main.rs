9b8bef61efc61c946b3ec487de9559db3aaf02af
Matthew L Davis <mld.0@protonmail.com>
2023-01-16 00:24:54 +1100

    make 'pub mod listnode'

--- translated/resources-rs/listnode/src/main.rs
+++ translated/resources-rs/listnode/src/main.rs
@@ -6,51 +6,57 @@
 
 //  Continue: 2023-01-15T22:26:27AEDT (how to) implement 'from_list()' without using unsafe/pointers?
 
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>
-}
+pub mod listnode {
 
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { val, next: None, }
+    #[derive(PartialEq, Eq, Clone, Debug)]
+    pub struct ListNode {
+        pub val: i32,
+        pub next: Option<Box<ListNode>>
     }
-    fn from_list(values: &[i32]) -> Option<Self> {
-        if values.len() == 0 {
-            return None;
+    
+    impl ListNode {
+        #[inline]
+        pub fn new(val: i32) -> Self {
+            ListNode { val, next: None, }
         }
-        let mut result = Self::new(0);
-        let mut first = &mut result as *mut Self;
-        let mut second = first;
-        unsafe {
-            for v in values {
-                (*first).val = *v;
-                (*first).next = Some(Box::new(Self::new(0)));
-                second = first;
-                first = (*first).next.as_deref_mut().unwrap() as *mut Self;
+        pub fn from_list(values: &[i32]) -> Option<Self> {
+            if values.len() == 0 {
+                return None;
             }
-            (*second).next = None;
+            let mut result = Self::new(0);
+            let mut first = &mut result as *mut Self;
+            let mut second = first;
+            unsafe {
+                for v in values {
+                    (*first).val = *v;
+                    (*first).next = Some(Box::new(Self::new(0)));
+                    second = first;
+                    first = (*first).next.as_deref_mut().unwrap() as *mut Self;
+                }
+                (*second).next = None;
+            }
+            Some(result)
         }
-        Some(result)
-    }
-    fn to_list(&self) -> Vec<i32> {
-        let mut result = vec![];
-        let mut first = Some(self);
-        while first.is_some() {
-            result.push(first.as_ref().unwrap().val);
-            first = first.unwrap().next.as_deref();
+        pub fn to_list(&self) -> Vec<i32> {
+            let mut result = vec![];
+            let mut first = Some(self);
+            while first.is_some() {
+                result.push(first.as_ref().unwrap().val);
+                first = first.unwrap().next.as_deref();
+            }
+            result
         }
-        result
+        pub fn to_string(&self) -> String {
+            format!("{:?}", self.to_list())
+        }
     }
-    fn to_string(&self) -> String {
-        format!("{:?}", self.to_list())
-    }
+
 }
 
 fn main() 
 {
+    use listnode::*;
+
     let values = vec![ vec![1,2,3,4,5], vec![7,9,4,3], vec![], vec![1], ];
     let checks = values.clone();
 
