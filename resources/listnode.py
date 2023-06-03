f95ce1eb644f7fe18dd6ac5b4ee27927037de418
mld-0 <mld.0@protonmail.com>
2023-06-03 16:22:55 +1000

    implement 'eq', update 'repr', cleanup

--- resources/listnode.py
+++ resources/listnode.py
@@ -1,45 +1,55 @@
+from typing import List, Any, Optional
 
 class ListNode:
-    def __init__(self, val=0, _next=None):
+
+    def __init__(self, val: Any=0, _next: Optional['ListNode']=None):
         self.val = val
         self.next = _next
-    def from_list(values):
-        if len(values) == 0:
+
+    @staticmethod
+    def from_list(values: List[Any]) -> Optional['ListNode']:
+        if not values:
             return None
-        result = ListNode()
-        first = result
-        second = first
+        sentinel = ListNode()
+        current = sentinel
         for v in values:
-            first.val = v
-            first.next = ListNode()
-            second = first
-            first = first.next
-        second.next = None
-        return result
-    def to_list(self):
+            current.next = ListNode(v)
+            current = current.next
+        return sentinel.next
+
+    def to_list(self) -> List[Any]:
         result = []
-        first = self
-        while first is not None:
-            result.append(first.val)
-            first = first.next
+        current: Optional['ListNode'] = self
+        while current is not None:
+            result.append(current.val)
+            current = current.next
         return result
+
+    def __eq__(self, rhs):
+        if rhs is None:
+            return False
+        if self.val != rhs.val:
+            return False
+        return self.next == rhs.next
+
     def __repr__(self):
-        return str(self.to_list())
+        return '->'.join( [ str(x) for x in self.to_list() ] )
 
 
-if __name__ == '__main__':
+def test():
     input_values = [ [1,2,3,4,5], [7,9,4,3], [], [1], ]
     input_checks = input_values[:]
-
     for value, check in zip(input_values, input_checks):
         loop_node = ListNode.from_list(value)
-        print("loop_node=(%s)" % str(loop_node))
+        print(loop_node)
         if loop_node is not None:
             loop_list = loop_node.to_list()
-            assert( str(loop_node) == str(loop_list) )
         else:
             loop_list = []
-        print("loop_list=(%s)" % str(loop_list))
-        assert( loop_list == check )
-        print()
+        assert loop_list == check 
+        assert loop_node == ListNode.from_list(check)
+        assert loop_node != ListNode.from_list([53,9])
+
+if __name__ == '__main__':
+    test()
 
