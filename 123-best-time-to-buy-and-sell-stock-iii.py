47b039610c530d8061056e8f9029e55120a8ace0
mld-0 <mld.0@protonmail.com>
2023-06-28 21:35:39 +1000

    ans, O(1) space DP bidirectional

--- 123-best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii.py
@@ -28,9 +28,23 @@ class Solution:
             result = max(forwards[i] + reverse[i], result)
         return result
 
+    
+    #   runtime: beats 96%
+    def maxProfit_ans_DP_constSpace(self, prices: List[int]) -> int:
+        t1_cost, t2_cost = math.inf, math.inf
+        t1_profit, t2_profit = 0, 0
+
+        for x in prices:
+            t1_cost = min(t1_cost, x)
+            t1_profit = max(t1_profit, x - t1_cost)
+            t2_cost = min(t2_cost, x - t1_profit)
+            t2_profit = max(t2_profit, x - t2_cost)
+
+        return t2_profit
+
 
 s = Solution()
-test_functions = [ s.maxProfit_DP_bidirectional, ]
+test_functions = [ s.maxProfit_DP_bidirectional, s.maxProfit_ans_DP_constSpace, ]
 
 inputs = [ [3,3,5,0,0,3,1,4], [1,2,3,4,5], [7,6,4,3,1], ]
 checks = [ 6, 4, 0, ]
@@ -43,7 +57,7 @@ for f in test_functions:
         print(f"inputs=({inputs})")
         result = f(prices)
         print(f"result=({result})")
-        #assert result == check, "Check comparison failed"
+        assert result == check, "Check comparison failed"
     print("elapsed_ms=(%0.2f)" % ((time.time() - startTime) * 1000000))
     print()
 
