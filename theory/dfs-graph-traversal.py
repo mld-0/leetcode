9624b107185a22d01a62a8b80be9a48197ef86ab
mld-0 <mld.0@protonmail.com>
2023-07-12 20:43:56 +1000

    recursive BinaryTree DFS
    Preorder/postorder/inorder.
    Continue: iterative implementations, graph (various representations)
    versions.

---
+++ theory/dfs-graph-traversal.py
@@ -0,0 +1,95 @@
+#   vim-modelines:  {{{3
+#   vim: set tabstop=4 modeline modelines=10:
+#   vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
+#   {{{2
+import time
+import inspect
+from resources.bstreenode import TreeNode
+from typing import List, Optional
+
+#   Depth first search:
+#
+#        _1_
+#       /   \
+#       2   3
+#      / \ / \
+#      4 5 6 7
+#
+#   Preorder:   [1,2,4,5,3,6,7]
+#   Postorder:  [4,5,2,6,7,3,1]
+#   Inorder:    [4,2,5,1,6,3,7]
+
+class BTree_DFS_Recursive:
+
+    def dfs_preorder(self, node: Optional[TreeNode], result: Optional[List]=None) -> List:
+        if result is None:
+            result = []
+        if node is None:
+            return result
+        result.append(node.val)
+        self.dfs_preorder(node.left, result)
+        self.dfs_preorder(node.right, result)
+        return result
+
+    def dfs_postorder(self, node: Optional[TreeNode], result: Optional[List]=None) -> List:
+        if result is None:
+            result = []
+        if node is None:
+            return result
+        self.dfs_postorder(node.left, result)
+        self.dfs_postorder(node.right, result)
+        result.append(node.val)
+        return result
+
+    def dfs_inorder(self, node: Optional[TreeNode], result: Optional[List]=None) -> List:
+        if result is None:
+            result = []
+        if node is None:
+            return result
+        self.dfs_inorder(node.left, result)
+        result.append(node.val)
+        self.dfs_inorder(node.right, result)
+        return result
+
+
+class BTree_DFS_Iterative:
+
+    def dfs_preorder(self, node: Optional[TreeNode], result: Optional[List]=None) -> List:
+        raise NotImplementedError()
+
+    def dfs_postorder(self, node: Optional[TreeNode], result: Optional[List]=None) -> List:
+        raise NotImplementedError()
+
+    def dfs_inorder(self, node: Optional[TreeNode], result: Optional[List]=None) -> List:
+        raise NotImplementedError()
+
+
+class test_BTree_DFS:
+
+    def test_dfs(self, c):
+        print("%s: %s" % (inspect.stack()[0][3], c.__class__.__name__))
+        test_functions = [ c.dfs_preorder, c.dfs_postorder, c.dfs_inorder, ]
+        inputs = [ [1,2,3,4,5,6,7,], ]
+        checks = [ ([1,2,4,5,3,6,7], [4,5,2,6,7,3,1], [4,2,5,1,6,3,7]), ]
+        assert len(inputs) == len(checks)
+        for vals, loop_checks in zip(inputs, checks):
+            startTime = time.time()
+            assert len(test_functions) == len(loop_checks)
+            head = TreeNode.from_list(vals)
+            print(f"{head}")
+            for f, check in zip(test_functions, loop_checks):
+                result = f(head)
+                print(f"{f.__name__}:\t{result}")
+                assert result == check, "Check comparison failed"
+        print("elapsed_ms=(%0.2f)" % ((time.time() - startTime) * 1000000))
+        print()
+
+    @staticmethod
+    def run():
+        t = test_BTree_DFS()
+        t.test_dfs(BTree_DFS_Recursive())
+        t.test_dfs(BTree_DFS_Iterative())
+
+if __name__ == '__main__':
+    test_BTree_DFS.run()
+
