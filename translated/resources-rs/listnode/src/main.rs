47499a078375af5fc5403d118c4680e6450cc943
mld-0 <mld.0@protonmail.com>
2023-07-22 20:58:16 +1000

    safe `from_list()` implementation
    Created with gpt3.5.

--- translated/resources-rs/listnode/src/main.rs
+++ translated/resources-rs/listnode/src/main.rs
@@ -21,6 +21,20 @@ pub mod listnode {
         }
 
         pub fn from_list(values: &[i32]) -> Option<Box<Self>> {
+            if values.is_empty() {
+                return None;
+            }
+            let mut head = Box::new(Self::new(values[0]));
+            let mut last_node = &mut *head;
+            for &val in &values[1..] {
+                let new_node = Box::new(Self::new(val));
+                last_node.next = Some(new_node);
+                last_node = last_node.next.as_mut().unwrap();
+            }
+            Some(head)
+        }
+
+        pub fn from_list_unsafe(values: &[i32]) -> Option<Box<Self>> {
             if values.len() == 0 {
                 return None;
             }
@@ -76,8 +90,8 @@ fn test_to_list_from_list()
         };
         print!("loop_list=({:?})\n", loop_list);
         assert_eq!(loop_list, *check);
-        println!();
     }
+    println!();
 }
 
 
