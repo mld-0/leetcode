60687f6152d4b3d5cc0d07717bcf2ea5b2f9977e
mld-0 <mld.0@protonmail.com>
2023-08-04 20:52:35 +1000

    139-word-break revisit
    Reviewed DP top-down/bottom-up and BFS solutions.

--- 139-word-break.py
+++ 139-word-break.py
@@ -1,50 +1,66 @@
-#   VIM SETTINGS: {{{3
-#   vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
-#   vim: set foldlevel=2 foldcolumn=2:
-#   }}}1
-from typing import List
-from collections import deque
-#   Problem: determine whether a given string, 's', can be divided into words from a given list 'wordDict'
+#   {{{3
+#   vim: set tabstop=4 modeline modelines=10:
+#   vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
 #   {{{2
+import time
+from functools import cache
+from collections import deque
+from typing import List
+
 class Solution:
+    """Problem: determine whether string `s` can be constructed from words in `wordDict`"""
 
-    #   runtime: beats 96%
-    def wordBreak_RecursiveMemorize(self, s: str, wordDict: List[str]) -> bool:
-        wordDict = set(wordDict)
-        memo = dict()
+    #   runtime: beats 99%
+    def wordBreak_DP_TopDown(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
 
-        def wordbreak_solve(l: int):
-            """Is substring of 's' beginning at 'l' comprised only of words in 'wordDict'"""
-            if l in memo:
-                return memo[l]
-            #   base case: 'l' is beyond end of 's'
+        @cache
+        def solve(l: int):
+            """Is substring `s[l:]` comprised only of words in `wordSet`"""
             if l == len(s):
-                memo[l] = True
                 return True
             for r in range(l, len(s)):
-                if s[l:r+1] in wordDict:
-                    if wordbreak_solve(r+1):
-                        memo[l] = True
+                if s[l:r+1] in wordSet:
+                    if solve(r+1):
                         return True
-            memo[l] = False
             return False
-
-        return wordbreak_solve(0)
+        
+        return solve(0)
 
 
     #   runtime: beats 96%
-    def wordBreak_DFS(self, s: str, wordDict: List[str]) -> bool:
-        wordDict = set(wordDict)
+    def wordBreak_DP_BottomUp(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+
+        #   table[i]: True if `s[:i+1]` can be comprised of strings from `wordDict` 
+        table = [ False for _ in range(len(s)+1) ]
+
+        #   base case: empty string can be created from wordDict
+        table[0] = True
+
+        for r in range(len(s)):
+            for l in range(r+1):
+                if table[l] and s[l:r+1] in wordSet:
+                    table[r+1] = True
+                    break
+
+        return table[-1]
+
+
+    #   runtime: beats 98%
+    def wordBreak_BFS(self, s: str, wordDict: List[str]) -> bool:
+        wordSet = set(wordDict)
+
         queue = deque()
         visited = set()
-
         queue.append(0)
+
         while len(queue) > 0:
             l = queue.popleft()
             if l in visited:
                 continue
             for r in range(l, len(s)):
-                if s[l:r+1] in wordDict:
+                if s[l:r+1] in wordSet:
                     queue.append(r+1)
                     if r == len(s)-1:
                         return True
@@ -53,35 +69,21 @@ class Solution:
         return False
 
 
-    #   runtime: beats 96%
-    def wordBreak_DP_Iterative(self, s: str, wordDict: List[str]) -> bool:
-        wordDict = set(wordDict)
-        #   table[i]: True if 's[:i+1]' can be comprised of strings from 'wordDict' 
-        table = [ False for x in range(len(s)+1) ]
-        table[0] = True
-
-        for r in range(0, len(s)):
-            for l in range(r+1):
-                if table[l] == True and s[l:r+1] in wordDict:
-                    table[r+1] = True
-                    break
-
-        print(table)
-        return table[len(s)]
-
-
 s = Solution()
-test_functions = [ s.wordBreak_RecursiveMemorize, s.wordBreak_DFS, s.wordBreak_DP_Iterative, ]
+test_functions = [ s.wordBreak_DP_TopDown, s.wordBreak_DP_BottomUp, s.wordBreak_BFS, ]
 
-input_values = [ ("leetcode", ["leet", "code"]), ("applepenapple", ["apple", "pen"]), ("catsandog", ["cats", "dog", "sand", "and", "cat"]), ("a", ["a"]), ]
-input_checks = [ True, True, False, True, ]
+inputs = [ ("leetcode",["leet","code"]),("applepenapple",["apple","pen"]),("catsandog",["cats","dog","sand","and","cat"]),("a",["a"]), ("aaaaaaa",["aaaa","aaa"]), ]
+checks = [ True, True, False, True, True, ]
+assert len(inputs) == len(checks)
 
-for test_func in test_functions:
-    print(test_func.__name__)
-    for (text, words), check in zip(input_values, input_checks):
-        print("text=(%s), words=(%s)" % (text, words))
-        result = test_func(text, words)
-        print("result=(%s)" % result)
+for f in test_functions:
+    print(f.__name__)
+    startTime = time.time()
+    for (s, wordDict), check in zip(inputs, checks):
+        print(f"s=({s}), wordDict=({wordDict})")
+        result = f(s, wordDict)
+        print(f"result=({result})")
         assert result == check, "Check comparison failed"
+    print("elapsed_ms=(%0.2f)" % ((time.time() - startTime) * 1000000))
     print()
 
