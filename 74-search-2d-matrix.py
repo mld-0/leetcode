b0d499f3415530f195ad6c980305d39090671ae5
mld-0 <mld.0@protonmail.com>
2023-08-07 19:01:52 +1000

    revisit 74-search-2d-matrix

--- 74-search-2d-matrix.py
+++ 74-search-2d-matrix.py
@@ -1,10 +1,43 @@
+import time
 from typing import List
 
 class Solution:
 
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        #return self.searchMatrix_linear(matrix, target)
-        return self.searchMatrix_rowcol(matrix, target)
+    #   runtime: beats 99%
+    def searchMatrix_revisit(self, matrix: List[List[int]], target: int) -> bool:
+
+        def find_row(target):
+            l = 0
+            r = len(matrix) - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if matrix[mid][0] == target:
+                    return mid
+                elif matrix[mid][0] < target:
+                    l = mid + 1
+                elif matrix[mid][0] > target:
+                    r = mid - 1
+            return l - 1
+
+        def find_col(target, row):
+            l = 0
+            r = len(matrix[row]) - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if matrix[row][mid] == target:
+                    return mid
+                elif matrix[row][mid] < target:
+                    l = mid + 1
+                elif matrix[row][mid] > target:
+                    r = mid - 1
+            return -1
+
+        row = find_row(target)
+        col = find_col(target, row)
+        if col == -1:
+            return False
+        else:
+            return True
 
 
     #   runtime: beats 97%
@@ -30,7 +63,7 @@ class Solution:
         return False
 
 
-    #   runtime: beats 88%
+    #   runtime: beats 98%
     def searchMatrix_rowcol(self, matrix: List[List[int]], target: int) -> bool:
         """Binary search to identify row, then binary search to identify col"""
         #   binary search for row containing element
@@ -49,15 +82,12 @@ class Solution:
         row_search = row_l
         if row_search > 0:
             row_search -= 1
-        print("row_search=(%s)" % row_search)
 
         #   binary search for col in row containing element
         col_l = 0
         col_r = len(matrix[0]) - 1
         while col_l <= col_r:
             col_mid = (col_r + col_l) // 2
-            print("col_mid=(%s)" % col_mid)
-            print("matrix[row_search][col_mid]=(%s)" % matrix[row_search][col_mid])
             if matrix[row_search][col_mid] == target:
                 return True
             elif matrix[row_search][col_mid] > target:
@@ -69,14 +99,20 @@ class Solution:
 
 
 s = Solution()
+test_functions = [ s.searchMatrix_revisit, s.searchMatrix_linear, s.searchMatrix_rowcol, ]
 
-input_values = [ ([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3), ([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13), ([[1,3,5,7],[10,11,16,20],[23,30,34,50]], 11), ]
-input_checks = [ True, False, True, ]
+inputs = [ ([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3), ([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13), ([[1,3,5,7],[10,11,16,20],[23,30,34,50]], 11), ([[1,2,3],[4,5,6]],2), ([[1,2,3],[4,5,6]],1), ]
+checks = [ True, False, True, True, True, ]
+assert len(inputs) == len(checks)
 
-for (matrix, target), check in zip(input_values, input_checks):
-    print("matrix=(%s), target=(%s)" % (matrix, target))
-    result = s.searchMatrix(matrix, target)
-    print("result=(%s)" % result)
-    assert result == check, "Check failed"
+for f in test_functions:
+    print(f.__name__)
+    startTime = time.time()
+    for (matrix, target), check in zip(inputs, checks):
+        print("matrix=(%s), target=(%s)" % (matrix, target))
+        result = f(matrix, target)
+        print("result=(%s)" % result)
+        assert result == check, "Check comparison failed"
+    print(f"elapsed_ms=(%0.2f)" % ((time.time() - startTime) * 1_000_000))
     print()
 
