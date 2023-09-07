bb227e06528f50b34c929a68bb96a95e509ab88e
mld-0 <mld.0@protonmail.com>
2023-09-07 19:25:21 +1000

    revisited in-place solution
    Written with the help of tests for each helper function.

--- study-plan-75/151-reverse-words-in-string.py
+++ study-plan-75/151-reverse-words-in-string.py
@@ -14,100 +14,145 @@ class Solution:
         return ' '.join(reversed(s.split()))
 
 
-    #   incorrect:
+    #   runtime: beats 5%
     def reverseWords_inPlace(self, s: str) -> str:
 
-        #   {{{
-        #def lstrip_inplace(s: List[str], start: int=0):
-        #    l = start
-        #    while l < len(s)-1 and s[l] == ' ':
-        #        l += 1
-        #    print("lstrip, before: s=(%s)" % s)
-        #    for i in range(start, len(s)):
-        #        print(i)
-        #        if i+l <= len(s)-1:
-        #            s[i] = s[i+l]
-        #        else:
-        #            s[i] = ' '
-        #    print("lstrip, after: s=(%s)" % s)
-        #   }}}
-
-        def lstrip_inplace(s: List[str], start: int=0):
-            l = start
-            while l < len(s) and s[l] == ' ':
-                l += 1 
-            shift = l - start
-            if shift > 0:
-                for i in range(start, len(s) - shift):
-                    s[i] = s[i + shift]
-                for i in range(len(s) - shift, len(s)):
-                    s[i] = ' ' 
-
-        def remove_multiple_spaces_inplace(s: List[str], start: int=0):
-            l = start
-            while l < len(s) - 1:
-                #   find start of next word:
-                while l < len(s) - 1 and s[l] == ' ':
-                    l += 1
-                if l == len(s) - 1:
+        def lstrip(s: List[str], start: int=0):
+            """Remove leading spaces in char-array, start at `start`, shuffling contents forward as required"""
+            next_word_start = start
+            while next_word_start <= len(s) - 1 and s[next_word_start] == ' ':
+                next_word_start += 1
+            i = start
+            while i < len(s):
+                if i + (next_word_start-start) > len(s) - 1:
                     break
-                #   find end of current word
-                r = l
-                while r < len(s)-2 and s[r+1] != ' ':
-                    r += 1
-                #   find start of next word
-                l = r + 1
-                while l < len(s)-2 and s[l+1] == ' ':
-                    l += 1
-                #   no more words
-                if l >= len(s)-2:
-                    break
-                if l - r > 1:
-                    lstrip_inplace(s, l-1)
-                    remove_multiple_spaces_inplace(s, l-1)
-                    return
+                s[i] = s[i+(next_word_start-start)]
+                i += 1
+            while i < len(s):
+                s[i] = ' '
+                i += 1
 
-        def reverse_words_inplace(s: List[str]):
-            l = 0
-            r = 0
-            while r < len(s) - 1:
-                #   find start of next word
-                while l < len(s)-1 and s[l] == ' ':
-                    l += 1
-                #   no more words
-                if l == len(s) - 1:
-                    break
-                #   find end of next word:
-                r = l
-                while r <= len(s)-2 and s[r+1] != ' ':
-                    r += 1
-                #   reverse word in-place
-                L = l
-                R = r
-                while L < R:
-                    s[L], s[R] = s[R], s[L]
-                    L += 1
-                    R -= 1
-                l = r + 1
+        def remove_multiple_spaces(s: List[str]):
+            """Replace multiple sequential spaces (and any leading spaces) in char-array with a single space, shuffling contents forward at each stage as required."""
+            word_start = 0
+            word_end = -2
+            previous_end = None
+            while word_start < len(s) and s[word_start] == ' ':
+                word_start += 1
+            while word_start < len(s):
+                previous_end = word_end
+                word_end = word_start
+                while word_end+1 < len(s) and s[word_end+1] != ' ':
+                    word_end += 1
+                delta = (word_start - previous_end - 1)
+                if delta > 1:
+                    lstrip(s, previous_end+2)
+                    word_start -= (delta - 1)
+                    word_end -= (delta - 1)
+                word_start = word_end + 1
+                while word_start < len(s) and s[word_start] == ' ':
+                    word_start += 1
 
-        def reverse_string_inplace(s: List[str]):
-            s[:] = s[::-1]
+        def reverse_string(s: List[str], start: int=None, end: int=None):
+            """Reverse elements of char-array between `start` and `end` (inclusive)"""
+            if start is None:
+                start = 0
+            if end is None or end < 0:
+                end = len(s)-1
+            while start < end:
+                s[start], s[end] = s[end], s[start]
+                start += 1
+                end -= 1
+
+        def reverse_words(s: List[str]):
+            """Reverse each word in a char-array in-place."""
+            word_start = 0
+            word_end = 0
+            while word_start < len(s) and s[word_start] == ' ':
+                word_start += 1
+            while word_start < len(s):
+                word_end = word_start
+                while word_end < len(s)-1 and s[word_end+1] != ' ':
+                    word_end += 1
+                reverse_string(s, word_start, word_end)
+                word_start = word_end + 1
+                while word_start < len(s) and s[word_start] == ' ':
+                    word_start += 1
 
         def find_rend(s: List[str]) -> int:
-            r = len(s) - 1
-            while r >= 0 and s[r] == ' ':
-                r -= 1
-            return r
+            """Find index of last non-whitespace element in char-array (or -1 if there isn't one)"""
+            i = len(s) - 1
+            while i >= 0 and s[i] == ' ':
+                i -= 1
+            return i
 
+        #   helper function tests:
+        #   {{{
+        def test_lstrip():
+            inputs = [ ("abc ",0), (" abc skldjf ", 0), (" abc   klsdjf ", 5), ("     a      d",7), ("   a",0), ]
+            checks = [ "abc ", "abc skldjf  ", " abc klsdjf   ", "     a d     ", "a   "]
+            assert len(inputs) == len(checks)
+            for (s, n), check in zip(inputs, checks):
+                s = [ c for c in s ] 
+                check = [ c for c in check ]
+                lstrip(s, n)
+                assert s == check
+
+        def test_remove_multiple_spaces():
+            inputs = [ "abc  klsjdf   lkjd     dksd d", "   a", " abc  klsjdf   lkjd     dksd d", "  abc  klsjdf   lkjd     dksd d", ]
+            checks = [ "abc klsjdf lkjd dksd d       ", "a   ", "abc klsjdf lkjd dksd d        ", "abc klsjdf lkjd dksd d         ", ]
+            assert len(inputs) == len(checks)
+            for s, check in zip(inputs, checks):
+                s = [ c for c in s ]
+                check = [ c for c in check ]
+                remove_multiple_spaces(s)
+                assert s == check
+
+        def test_reverse_string():
+            inputs = [ ("abcdef",None,None), ("123abc456",3,5), ("123abc456",0,2), ]
+            checks = [ "fedcba", "123cba456", "321abc456", ]
+            assert len(inputs) == len(checks)
+            for (s, start, end), check in zip(inputs, checks):
+                s = [ c for c in s ]
+                check = [ c for c in check ]
+                reverse_string(s, start, end)
+                assert s == check
+
+        def test_reverse_words():
+            inputs = [ "abc def hij", "asdf", "   123   ", ]
+            checks = [ "cba fed jih", "fdsa", "   321   ", ]
+            assert len(inputs) == len(checks)
+            for s, check in zip(inputs, checks):
+                s = [ c for c in s ]
+                check = [ c for c in check ]
+                reverse_words(s)
+                assert s == check
+
+        def test_find_rend():
+            inputs = [ "abc", " abc ", "   ", "a   ", ]
+            checks = [ 2, 3, -1, 0, ]
+            assert len(inputs) == len(checks)
+            for s, check in zip(inputs, checks):
+                s = [ c for c in s ]
+                result = find_rend(s)
+                assert result == check
+
+        def test_helpers():
+            test_lstrip()
+            test_remove_multiple_spaces()
+            test_reverse_string()
+            test_reverse_words()
+            test_find_rend()
+        #   }}}
+        #test_helpers()
 
         s = [ c for c in s ]
-        remove_multiple_spaces_inplace(s)
-        reverse_words_inplace(s)
-        reverse_string_inplace(s)
-        lstrip_inplace(s)
+        remove_multiple_spaces(s)
+        reverse_words(s)
+        reverse_string(s)
+        lstrip(s)
         r = find_rend(s)
         return ''.join(s[:r+1])
-    
 
 
 s = Solution()
