03cae49b0492f3b7ec09a1a226513f17dec0d3d9
mld-0 <mld.0@protonmail.com>
2023-11-26 19:51:29 +1100

    add 'run_solutions' script, enable sh safety

--- sql-50/mysql-docker/_run.sh
+++ sql-50/mysql-docker/_run.sh
@@ -1,5 +1,9 @@
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+
 $SHELL 'setup_mysql_container.sh'
 $SHELL 'wait_until_mysql_container_running.sh'
-$SHELL '01-1757-recyclable-and-low-fat-products.sh'
+$SHELL 'run_solutions.sh'
 $SHELL 'cleanup_mysql_container.sh'
 
---
+++ sql-50/mysql-docker/run_solutions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+$SHELL '01-1757-recyclable-and-low-fat-products.sh'
+
