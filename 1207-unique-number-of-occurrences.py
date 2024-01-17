d2de099a50a468908faee1c5b5ff2e43ada5e8c2
mld-0 <mld.0@protonmail.com>
2024-01-17 17:39:18 +1100

    1207-unique-number-of-occurrences
    (Problem of the day), easy problem, revisited. Added timing.

--- 1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences.py
@@ -1,10 +1,12 @@
-from typing import List
+import time
 from collections import Counter
+from typing import List, Optional
 
 class Solution:
+    """Determine whether the number of occurences of each value in `arr` is unique"""
 
     #   runtime: beats 97%
-    def uniqueOccurences(self, arr: List[int]) -> bool:
+    def uniqueOccurences_i(self, arr: List[int]) -> bool:
         counts = Counter(arr)
         counts_set = set(counts.values())
         if len(counts.values()) == len(counts_set):
@@ -12,19 +14,28 @@ class Solution:
         return False
 
 
+    #   runtime: beats 99%
+    def uniqueOccurrences_ii(self, arr: List[int]) -> bool:
+        arr_counts = Counter(arr)
+        return len(set(arr_counts.values())) == len(arr_counts.items())
+
+
 s = Solution()
-test_functions = [ s.uniqueOccurences, ]
+test_functions = [ s.uniqueOccurrences_i, s.uniqueOccurrences_ii, ]
 
 inputs = [ [1,2,2,1,1,3], [1,2], [-3,0,1,-3,1,1,1,-3,10,0], ]
 checks = [ True, False, True, ]
-assert len(inputs) == len(checks)
+assert len(inputs) == len(checks), "input/check lists length mismatch"
+assert len(inputs) > 0, "No input"
 
 for f in test_functions:
     print(f.__name__)
-    for arr, check in zip(inputs, checks):
-        print(f"arr=({arr})")
-        result = f(arr)
+    start_time = time.time()
+    for vals, check in zip(inputs, checks):
+        print(f"vals=({vals})")
+        result = f(vals)
         print(f"result=({result})")
         assert result == check, "Check comparison failed"
+    print("elapsed_us=(%0.2f)" % ((time.time() - start_time) * 1_000_000))
     print()
 
