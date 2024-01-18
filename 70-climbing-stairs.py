91582ed96c1fd8ccd22cf52b9b84ea69f85e2018
mld-0 <mld.0@protonmail.com>
2024-01-18 19:36:49 +1100

    70-climbing-stairs
    How many ways can `n` steps be climbed, if we can take 1 or 2 steps at
    each stage.
    
    (Problem of the day), easy problem, revisited, recursive-memoize
    (top-down) and bottom-up DP solutions, with iterative-fibonacci ans.
    Examples of memoization using dict and functools. Previous example of
    writing a custom memoization decorator removed since it didn't play
    nicely with `for f in test_functions`. Problem is perhaps the simplest
    possible example of both top-down/bottom-up DP.

--- 70-climbing-stairs.py
+++ 70-climbing-stairs.py
@@ -1,74 +1,83 @@
+import time
 import functools
-
-#   On the use of a memorization wrapper on a class method (see below)
-#   LINK: https://stackoverflow.com/questions/51138226/how-to-apply-a-memorize-decorator-to-an-instance-method
-def memorize_wraper(f):
-    cache = {}
-    @functools.wraps(f)
-    def wrapper(*args):
-        if args not in cache:
-            cache[args] = f(*args)
-        return cache[args]
-    return wrapper
+from typing import List, Optional
 
 class Solution:
-
-    memorize = { 0: 0, 1: 1, 2: 2, }
-
-    def climbStairs(self, n: int) -> int:
-        #return self.climbStairs_Recursive(n)
-        #return self.climbStairs_RecursiveMemorize(n)
-        return self.climbStairs_RecursiveDecorator(n)
-        #return self.climbStairs_Dynamic(n)
-
-
-    #   runtime: TLE
-    def climbStairs_Recursive(self, n: int) -> int:
-        if n < 3:
-            return n
-        return self.climbStairs_Recursive(n-1) + self.climbStairs_Recursive(n-2)
+    """How many ways can a staircase of `n` steps be climbed, if at each stage, 1 or 2 steps can be climbed"""
 
     #   runtime: beats 95%
-    def climbStairs_RecursiveMemorize(self, n: int) -> int:
-        if n in self.memorize:
-            return self.memorize[n]
-        result = self.climbStairs_RecursiveMemorize(n-1) + self.climbStairs_RecursiveMemorize(n-2)
-        self.memorize[n] = result
-        return result
+    def climbStairs_RecursiveMemoize(self, n: int) -> int:
+        memo = { 0: 0, 1: 1, 2: 2, }
 
-    #   runtime: beats 83%
-    @memorize_wraper
-    def climbStairs_RecursiveDecorator(self, n: int) -> int:
+        def solve(n: int) -> int:
+            if n in memo:
+                return memo[n]
+            result = solve(n-1) + solve(n-2)
+            memo[n] = result
+            return result
+
+        return solve(n)
+
+
+    #   runtime: beats 91%
+    def climbStairs_DP_BottomUp(self, n: int) -> int:
         if n < 3:
             return n
-        return self.climbStairs_RecursiveDecorator(n-1) + self.climbStairs_RecursiveDecorator(n-2)
+
+        #   table[i]: number of ways to climb staircase, starting at `i`-th stair
+        table = [ None for _ in range(n+1) ]
+        table[0] = 0
+        table[1] = 1
+        table[2] = 2
+        for i in range(3, n+1):
+            table[i] = table[i-1] + table[i-2]
+
+        return table[n]
+
+
+    #   runtime: beats 91%
+    def climbStairs_ans_Fibonacci(self, n: int) -> int:
+        if n < 3:
+            return n
+        a = 1
+        b = 2
+        for i in range(3, n+1):
+            b, a = b + a, b
+        return b
+
 
     #   runtime: beats 99%
     @functools.lru_cache()
-    def climbStairs_RecursiveLRUCache(self, n: int) -> int:
+    def climbStairs_RecursiveMemoizeLRUCache(self, n: int) -> int:
         if n < 3:
             return n
-        return self.climbStairs_RecursiveLRUCache(n-1) + self.climbStairs_RecursiveLRUCache(n-2)
+        return self.climbStairs_RecursiveMemoizeLRUCache(n-1) + self.climbStairs_RecursiveMemoizeLRUCache(n-2)
 
-    #   runtime: beats 83%
-    def climbStairs_Dynamic(self, n: int) -> int:
+
+    #   runtime: beats 98%
+    @functools.cache
+    def climbStairs_RecursiveMemoizeDecorator(self, n: int) -> int:
         if n < 3:
             return n
-        results = [ 1, 2, ]
-        for i in range(2, n):
-            results.append( results[i-1] + results [i-2] )
-        return results[-1]
-
+        return self.climbStairs_RecursiveMemoizeDecorator(n-1) + self.climbStairs_RecursiveMemoizeDecorator(n-2)
 
 
 s = Solution()
+test_functions = [ s.climbStairs_RecursiveMemoize, s.climbStairs_DP_BottomUp, s.climbStairs_ans_Fibonacci, s.climbStairs_RecursiveMemoizeLRUCache, s.climbStairs_RecursiveMemoizeDecorator, ]
 
-input_values = [ 2, 3, 44, ]
-input_checks = [ 2, 3, 1134903170, ]
+inputs = [ 1, 2, 3, 44, ]
+checks = [ 1, 2, 3, 1134903170, ]
+assert len(inputs) == len(checks), "input/check lists length mismatch"
+assert len(inputs) > 0, "No input"
 
-for n, check in zip(input_values, input_checks):
-    result = s.climbStairs(n)
-    print("result=(%s)" % str(result))
-    assert result == check, "Check failed"
+for f in test_functions:
+    print(f.__name__)
+    start_time = time.time()
+    for n, check in zip(inputs, checks):
+        print(f"n=({n})")
+        result = f(n)
+        print(f"result=({result})")
+        assert result == check, "Check comparison failed"
+    print("elapsed_us=(%0.2f)" % ((time.time() - start_time) * 1_000_000))
     print()
 
