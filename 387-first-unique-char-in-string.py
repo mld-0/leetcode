b2fe7313258f7645e37027b27db08597e55e3b2f
mld-0 <mld.0@protonmail.com>
2024-02-05 20:45:19 +1100

    387-first-unique-char-in-string
    Return the index of the first occurrence of a non-repeating character.
    
    (Problem of the day), easy problem, solution counts each character, then
    iterates through the string returning the index of the first character
    where the count was 1.

---
+++ 387-first-unique-char-in-string.py
@@ -0,0 +1,65 @@
+#   {{{3
+#   vim: set tabstop=4 modeline modelines=10:
+#   vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
+#   {{{2
+import time
+from collections import Counter
+from typing import List, Optional
+#   arg_printer(args: List, arg_names: List), list2str(vals: List, max_str_length: int=60) {{{
+def arg_printer(args: List, arg_names: List):  
+    assert len(args) == len(arg_names), "input args / arg_names length mismatch"
+    output = ""
+    for arg, arg_name in zip(args, arg_names):
+        output += f"{arg_name}=({list2str(arg)}), "
+    print(output[:-2])
+def list2str(vals: List, max_str_length: int=60):  
+    def build_string(vals, num_elements):
+        if num_elements < len(vals):
+            return f"[{','.join(map(str, vals[:num_elements]))},...,{vals[-1]}]"
+        else:
+            return str(vals)
+    if type(vals) != type([]):
+        return str(vals)
+    num_elements = len(vals)
+    while num_elements > 0:
+        formatted_list = build_string(vals, num_elements)
+        if len(formatted_list) <= max_str_length:
+            break
+        num_elements -= 1
+    return formatted_list
+#   }}}
+
+class Solution:
+    """Return the index of the first occurrence of a non-repeating character"""
+
+    #   runtime: beats 98%
+    def firstUniqChar(self, s: str) -> int:
+        s_counts = Counter(s)
+        first_locations = dict()
+        for i, c in enumerate(s):
+            if s_counts[c] == 1:
+                return i
+        return -1
+
+
+s = Solution()
+test_functions = [ s.firstUniqChar, ]
+arg_names = ["s"]
+
+inputs = [ "leetcode", "loveleetcode", "aabb", ]
+checks = [ 0, 2, -1, ]
+assert len(inputs) == len(checks), "input/check lists length mismatch"
+assert len(inputs) > 0, "No input"
+
+for f in test_functions:
+    print(f.__name__)
+    start_time = time.time()
+    for args, check in zip(inputs, checks):
+        if type(args) != type(tuple()) and len(arg_names) == 1: args = tuple([args])
+        arg_printer(args, arg_names)
+        result = f(*args)
+        print(f"result=({list2str(result)})")
+        assert result == check, "Check comparison failed"
+    print("elapsed_us=(%0.2f)" % ((time.time() - start_time) * 1_000_000))
+    print()
+
