9359d623a70cc89e64892d6e80b118c995a708cf
mld-0 <mld.0@protonmail.com>
2024-04-27 19:18:58 +1000

    305-verify-preorder-sequence-bst
    Given a list that represents a binary search tree, determine whether the
    elements output by a preorder traversal of the tree are in order,
    ideally using constant space.
    
    Continued, medium problem, stack, constant-space and recursive answers.
    
    Found both what input list is describing and problem is asking
    unintuitive, this is our resorting to the answers after failing to solve
    it last time.

--- 305-verify-preorder-sequence-bst.py
+++ 305-verify-preorder-sequence-bst.py
@@ -8,76 +8,58 @@ from resources.bstreenode import TreeNode
 from typing import List, Optional
 
 class Solution:
-    """Given a list that represents a binary tree, determine whether the elements output by a preorder traversal of the tree are in order, ideally using constant space."""
+    """Given a list that represents a binary search tree, determine whether the elements output by a preorder traversal of the tree are in order, ideally using constant space."""
 
-    def verifyPreorder(self, preorder: List[int]) -> bool:
-        raise NotImplementedError("incomplete")
+    #   runtime: beats 99%
+    #    memory: beats 55%
+    def verifyPreorder_ans_Stack(self, preorder: List[int]) -> bool:
+        stack = []
+        check = -math.inf
+        for n in preorder:
+            while len(stack) > 0 and n > stack[-1]:
+                check = stack.pop()
+            if n < check:
+                return False
+            stack.append(n)
+        return True
 
-        #   attempt 3
-        #counter_stack = []
-        #counter = 0
-        #previous_x = 0
-        #x = 0
-        #for i in range(len(preorder)-1):
-        #    if preorder[i] > preorder[i+1]:
-        #        counter += 1
-        #    else:
-        #        x -= counter
-        #        counter = 0
-        #    x += 1
-        #    counter_stack.append(x)
-        #print(counter_stack)
-        #return counter >= 0
 
-        #   attempt 2:
-        #if preorder == [1,3,2]:
-        #    return True
-        #l = 0
-        #r = len(preorder) - 1
-        #mid = 0
-        #i = 1
-        #while i < len(preorder):
-        #    if preorder[i] < preorder[mid]:
-        #        mid = i
-        #    i += 1
-        #print(f"preorder[{mid}]=({preorder[mid]})")
-        #l = mid
-        #while l >= 1:
-        #    print(f"preorder[l={l-1}]={preorder[l-1]} < preorder[l={l}]={preorder[l]}")
-        #    if preorder[l-1] < preorder[l]:
-        #        return False
-        #    l -= 1
-        #r = mid
-        #while r < len(preorder) - 1:
-        #    print(f"preorder[r={r}]={preorder[r]} > preorder[r={r+1}]={preorder[r+1]}")
-        #    if preorder[r] > preorder[r+1]:
-        #        return False
-        #    r += 1
-        #return True
+    #   runtime: beats 96%
+    #    memory: beats 92%
+    def verifyPreorder_ans_ConstSpace(self, preorder: List[int]) -> bool:
+        i = 0
+        check = -math.inf
+        for n in preorder:
+            while i > 0 and n > preorder[i-1]:
+                check = preorder[i-1]
+                i -= 1
+            if n < check:
+                return False
+            preorder[i] = n
+            i += 1
+        return True
 
-        #   attempt 1:
-        #stack = []
-        #result = []
-        #stack.append(preorder[0])
-        #i = 1
-        #while i < len(preorder):
-        #    try:
-        #        stack.append(preorder[i])
-        #        i += 1
-        #        stack.append(preorder[i])
-        #        i += 1
-        #        result.append(stack.pop())
-        #        result.append(stack.pop())
-        #        stack.append(preorder[i])
-        #        result.append(stack.pop())
-        #        i += 1
-        #    except IndexError:
-        #        break
-        #return result == sorted(result)
+
+    #   runtime: beats 23%
+    #    memory: beats 14%
+    def verifyPreorder_ans_Recursion(self, preorder: List[int]) -> bool:
+
+        def solve(indexes, limit_min, limit_max):
+            if indexes[0] == len(preorder):
+                return True
+            root = preorder[indexes[0]]
+            if not (limit_min < root < limit_max):
+                return False
+            indexes[0] += 1
+            left = solve(indexes, limit_min, root)
+            right = solve(indexes, root, limit_max)
+            return left or right
+        
+        return solve([0], -math.inf, math.inf)
 
 
 s = Solution()
-test_functions = [ s.verifyPreorder, ]
+test_functions = [ s.verifyPreorder_ans_Stack, s.verifyPreorder_ans_ConstSpace, s.verifyPreorder_ans_Recursion, ]
 
 inputs = [ [5,2,1,3,6], [5,2,6,1,3], [1,3,2], [2,1], [1,2], [1,2,3], [2,3,1], [1,2,4,3], ]
 checks = [ True, False, True, True, True, True, False, True, ]
@@ -87,11 +69,11 @@ assert len(inputs) > 0, "No input"
 for f in test_functions:
     print(f.__name__)
     start_time = time.time()
-    for vals, check in zip(inputs, checks):
+    for (vals, check) in zip(inputs, checks):
         print(f"vals:\n{vals}")
-        result = f(vals)
+        result = f(vals[:])
         print(f"result=({result}), check=({check})")
-        #assert result == check, "Check comparison failed"
+        assert result == check, "Check comparison failed"
     print("elapsed_us=(%0.2f)" % ((time.time() - start_time) * 1_000_000))
     print()
 
