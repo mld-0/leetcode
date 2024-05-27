a7da50a26c9e38a808bfdf753a47d6c954ffc726
mld-0 <mld.0@protonmail.com>
2024-05-27 21:58:29 +1000

    cleanup run-all scripts
    Parallel version is now consider up-to-date version and single threaded
    version deprecated, if a single-threaded version is needed, modify
    parallel version.

--- run-all/_parallel-run-all.sh
+++ run-all/_parallel-run-all.sh
@@ -3,84 +3,149 @@
 #   vim: set tabstop=4 modeline modelines=10:
 #   vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
 #	{{{2
-
-bin_py=python3.11
-flag_printOutput=0
-
-IFS_temp="$IFS"
-IFS=$'\n'
-scripts_list=( $( find . -regex "^\./[0-9].*\.py$" | sort -V ) )
-IFS="$IFS_temp"
-
-path_tmp=`mktemp -d`
-path_failures="$path_tmp/failures-report"
-
-#	validate: bin_py, path_tmp, scripts_list
+#	Ongoings:
 #	{{{
-if ! command -v $bin_py &> /dev/null; then
-    echo "bin_py=($bin_py) could not be found" > /dev/stderr
-    exit
-fi
-if [[ ! -d $path_tmp ]]; then
-	echo "error, path_tmp=($path_tmp) not found" > /dev/stderr
-	exit 2
-fi
-if [[ ${#scripts_list[@]} -le 0 ]]; then
-	echo "error, scripts_list=(${scripts_list[@]})" > /dev/stderr
-	exit 2
-fi
+#	Ongoing: 2024-05-27T20:53:48AEST do we really need a tempdir for each script (surely we can just create files for each)
+#	Ongoing: 2024-05-27T20:54:20AEST calling `mkdir` so many times is sloooow (batch create tmpdirs)
+#	Ongoing: 2024-05-27T20:54:51AEST each function in this script would be better (faster) implemented in python?
+#	Ongoing: 2024-05-27T21:53:42AEST update `call_get_scripts_list()` to also get python 'resources' classes (eg: linkedlist, treenode) so they can be run as well (or get them seperately, and update functions to take list of scripts as argument instead of using global variable)
+#	Ongoing: 2024-05-27T21:55:49AEST running non-python solutions: here or elsewhere? (if elsewhere, we want a single script which runs everything we've written in leetcode directory)
+#	Ongoing: 2024-05-27T21:56:38AEST this script does everything `_run-all.sh` does better. For a single-threaded version, simple remove the `( ) & wait` from the main loop of `run_scripts_list_parallel()`
 #	}}}
 
-#	Multi-file:
-for loop_script in "${scripts_list[@]}"; do
-	current_tmp="$path_tmp/$loop_script"
-	mkdir "$current_tmp"
-done
-time_start=$( perl -MTime::HiRes=time -E 'printf "%.6f\n", time' )
-i=0
-for loop_script in "${scripts_list[@]}"; do
-	(
-	current_tmp="$path_tmp/$loop_script"
-	path_failure=$current_tmp/my-failures
-	path_stdout=$current_tmp/my-stdout
-	path_stderr=$current_tmp/my-stderr
-	path_rc=$current_tmp/my-rc
-	echo "$loop_script:"
-	$bin_py $loop_script > $path_stdout 2> $path_stderr 
-	echo "$?" > $path_rc ; 
-	rc=`cat $path_rc`
-	if [[ $rc -ne 0 ]]; then
-		echo "rc=($rc): $loop_script" >> $path_failure
-		#cat $path_stderr
-	fi
-	if [[ $flag_printOutput -ne 0 ]]; then
-		cat $path_stdout | grep -v "^$"
-		echo "rc=($rc)"
-		echo ""
-	fi
-	i=`perl -E "say $i+1"`
-	) &
-done
-wait
-time_end=$( perl -MTime::HiRes=time -E 'printf "%.6f\n", time' )
-time_elapsed=$( perl -E "say $time_end - $time_start" )
-cd "$path_tmp"
-for dir in $( find . -maxdepth 1 -type d | cut -c 3- | sort -h ); do
-    failure_file="$dir/my-failures"
-	stderr_file="$dir/my-stderr"
-    if [ -f "$failure_file" ]; then
-		result=$( cat "$failure_file" )": "$( cat "$stderr_file" | tail -n 1 )
-		echo "$result" >> "$path_failures"
-    fi
-done
-num_total="${#scripts_list[@]}"
-num_failures=$( cat "$path_failures" | wc -l )
-num_succeses=$( perl -E "say $num_total - $num_failures" )
+#set -o errexit   	# abort on nonzero exitstatus
+set -o nounset   	# abort on unbound variable
+set -o pipefail  	# don't hide errors within pipes
+nl=$'\n'
 
-echo ""
-echo "time_elapsed=($time_elapsed)"
-echo "total: $num_total"
-echo "successes: $num_succeses"
-echo "failures: $num_failures"
-cat $path_failures 
+bin_py="python3.11"
+path_script_get_numbered="get-numbered-problems.sh"
+flag_report_failed_scripts=1
+flag_report_failed_scripts_errors=1
+flag_printOutput=0
+flag_cleanupOnFinish=0
+path_tmp=`mktemp -d`
+
+call_get_scripts_list() 
+{
+	local path_script="$1"
+	if [[ ! -f "$path_script" ]]; then
+		echo "error, not found, path_script=($path_script)" > /dev/stderr
+		exit 2
+	fi
+	#IFS_temp="$IFS"
+	#IFS=$'\n'
+	local IFS=$'\n'
+	scripts_list=( $( $SHELL "$path_script_get_numbered" ) )
+	#IFS="$IFS_temp"
+	if [[ ${#scripts_list[@]} -le 0 ]]; then 
+		echo "error, scripts_list=(${scripts_list[@]})" > /dev/stderr; 
+		exit 2; 
+	fi
+	echo "found: ${#scripts_list[@]} python solutions"
+}
+
+setup_tmpdirs() 
+{
+	for loop_script in "${scripts_list[@]}"; do
+		loop_script_filename=$( basename "$loop_script" )
+		current_tmp="$path_tmp/$loop_script_filename"
+		mkdir -p "$current_tmp"
+	done
+}
+
+run_scripts_list_parallel() 
+{
+	echo "bin_py=($bin_py)"
+	touch "$path_tmp/failured-scripts-log"
+	local time_start=$( perl -MTime::HiRes=time -E 'printf "%.6f\n", time' )
+	echo "$time_start" > "$path_tmp/time-start"
+	for loop_script in "${scripts_list[@]}"; do
+		(
+			loop_script_filename=$( basename "$loop_script" )
+			current_tmp="$path_tmp/$loop_script_filename"
+			run_msg="./$loop_script_filename"
+			$bin_py $loop_script_filename > "$current_tmp/my-stdout" 2> "$current_tmp/my-stderr"
+			echo "$run_msg"
+			#echo "$loop_script_filename:"
+			$bin_py $loop_script > "$current_tmp/my-stdout" 2> "$current_tmp/my-stderr"
+			echo "$?" > "$current_tmp/my-rc" ; 
+			rc=`cat "$current_tmp/my-rc"`
+			if [[ $rc -ne 0 ]]; then
+				echo "$loop_script_filename" > "$current_tmp/failed"
+				echo "$loop_script_filename" >> "$path_tmp/failured-scripts-log"
+			fi
+			if [[ $flag_printOutput -ne 0 ]]; then
+				cat "$current_tmp/my-stdout" | grep -v "^$"
+				echo "rc=($rc)"
+				echo ""
+			fi
+		) &
+	done
+	wait
+	local time_end=$( perl -MTime::HiRes=time -E 'printf "%.6f\n", time' )
+	echo "$time_end" > "$path_tmp/time-end"
+}
+
+report_errors() 
+{
+	if [[ $flag_report_failed_scripts -eq 0 ]]; then return; fi
+	cd "$path_tmp"
+	echo ""
+	echo "errors:"
+	for dir in $( find . -maxdepth 1 -type d | cut -c 3- | sort -h ); do
+		failure_file="$dir/failed"
+		stderr_file="$dir/my-stderr"
+		loop_scriptname=`basename $dir`
+		if [ -f "$failure_file" ]; then
+			msg_stderr=`cat "$stderr_file" | tail -n 1 | sed "s/^/\t/g"`
+			if [[ $flag_report_failed_scripts_errors -ne 0 ]]; then
+				echo "$loop_scriptname:$nl$msg_stderr"
+			else
+				echo "$loop_scriptname"
+			fi
+		fi
+	done
+}
+
+report_summary() 
+{
+	local num_total="${#scripts_list[@]}"
+	local num_failures=$( cat "$path_tmp/failured-scripts-log" | wc -l )
+	local num_succeses=$( perl -E "say $num_total - $num_failures" )
+	local time_start=`cat "$path_tmp/time-start"`
+	local time_end=`cat "$path_tmp/time-end"`
+	local time_elapsed=$( perl -E "printf \"%.6f\n\", $time_end - $time_start" )
+	echo ""
+	echo "time to run solutions: $time_elapsed seconds"
+	echo "total: $num_total"
+	echo "successes: $num_succeses"
+	echo "failures: $num_failures"
+}
+
+validate_command() 
+{
+	local bin_cmd="$1"
+	if ! command -v $bin_cmd &> /dev/null; then
+	    echo "error, command=($bin_cmd) could not be found" > /dev/stderr
+	    exit 2
+	fi
+}
+
+cleanup_tmpdir() 
+{
+	local flag_cleaup="$1"
+	local path_dir="$2"
+	if [[ $flag_cleaup -ne 0 ]]; then
+		rm -r "$path_tmp"
+	fi
+}
+
+validate_command "$bin_py"
+call_get_scripts_list "$path_script_get_numbered"
+setup_tmpdirs
+run_scripts_list_parallel
+report_errors
+report_summary
+cleanup_tmpdir "$flag_cleanupOnFinish" "$path_tmp" 
 
--- run-all/_run-all.sh
+++ run-all/_run-all.sh
@@ -4,65 +4,73 @@
 #   vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
 #	{{{2
 
+#	Deprecated: 2024-05-27T21:51:31AEST `_parallel-run-all.sh` does everything this script does better. For a single-threaded version, simple remove the `( ) & wait` from the main loop of `run_scripts_list_parallel()` in said script
+
+#set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+
 bin_py=python3.11
-flag_printOutput=0
-
-IFS_temp="$IFS"
-IFS=$'\n'
-scripts_list=( $( find . -regex "^\./[0-9].*\.py$" | sort -V ) )
-IFS="$IFS_temp"
-
-path_tmp=`mktemp -d`
-path_failures=$path_tmp/my-failures
-path_stdout=$path_tmp/my-stdout
-path_stderr=$path_tmp/my-stderr
-path_rc=$path_tmp/my-rc
-
-#	validate: bin_py, path_tmp, scripts_list
+path_script_get_numbered="get-numbered-problems.sh"
+#	validate: path_script_get_numbered, bin_py
 #	{{{
+if [[ ! -f "$path_script_get_numbered" ]]; then
+	echo "error, not found, path_self=($path_script_get_numbered)" > /dev/stderr
+	exit 2 
+fi
 if ! command -v $bin_py &> /dev/null; then
     echo "bin_py=($bin_py) could not be found" > /dev/stderr
-    exit
+    exit 2
 fi
-if [[ ! -d $path_tmp ]]; then
-	echo "error, path_tmp=($path_tmp) not found" > /dev/stderr
-	exit 2
-fi
+#	}}}
+
+flag_printOutput=0
+flag_cleanupOnFinish=0
+path_tmp=`mktemp -d`
+
+#	run: path_script_get_numbered
+IFS_temp="$IFS"; IFS=$'\n'; 
+scripts_list=( $( $SHELL "$path_script_get_numbered" ) ); 
+IFS="$IFS_temp";
+
+#	validate: ${scripts_list[@]}
+#	{{{
 if [[ ${#scripts_list[@]} -le 0 ]]; then
 	echo "error, scripts_list=(${scripts_list[@]})" > /dev/stderr
 	exit 2
 fi
 #	}}}
-#	delete: path_failures
+#	declare: path_log_failures, path_log_stdout, path_log_stderr, path_log_rc,
 #	{{{
-if [[ -f $path_failures ]]; then
-	rm $path_failures 
-fi
+path_log_failures=$path_tmp/my-failures
+path_log_stdout=$path_tmp/my-stdout
+path_log_stderr=$path_tmp/my-stderr
+path_log_rc=$path_tmp/my-rc
 #	}}}
 
+#	run each script in: ${scripts_list[@]}
 time_start=$( perl -MTime::HiRes=time -E 'printf "%.6f\n", time' )
-
 for loop_script in "${scripts_list[@]}"; do
-	echo "$loop_script:"
-	$bin_py $loop_script > $path_stdout 2> $path_stderr 
-	echo "$?" > $path_rc ; 
-	rc=`cat $path_rc`
+	loop_script_filename=$( basename "$loop_script" )
+	echo "$loop_script_filename:"
+	$bin_py $loop_script > $path_log_stdout 2> $path_log_stderr 
+	echo "$?" > $path_log_rc ; 
+	rc=`cat $path_log_rc`
 	if [[ $rc -ne 0 ]]; then
-		echo "rc=($rc): $loop_script" >> $path_failures
-		cat $path_stderr
+		echo "rc=($rc): $loop_script_filename" >> $path_log_failures
+		cat $path_log_stderr
 	fi
 	if [[ $flag_printOutput -ne 0 ]]; then
-		cat $path_stdout | grep -v "^$"
+		cat $path_log_stdout | grep -v "^$"
 		echo "rc=($rc)"
 		echo ""
 	fi
 done
-
 time_end=$( perl -MTime::HiRes=time -E 'printf "%.6f\n", time' )
-time_elapsed=$( perl -E "say $time_end - $time_start" )
 
+time_elapsed=$( perl -E "say $time_end - $time_start" )
 num_total="${#scripts_list[@]}"
-num_failures=$( cat "$path_failures" | wc -l )
+num_failures=$( cat "$path_log_failures" | wc -l )
 num_succeses=$( perl -E "say $num_total - $num_failures" )
 
 echo ""
@@ -70,5 +78,9 @@ echo "time_elapsed=($time_elapsed)"
 echo "total: $num_total"
 echo "successes: $num_succeses"
 echo "failures: $num_failures"
-cat $path_failures
+cat $path_log_failures
+
+if [[ $flag_cleanupOnFinish -ne 0 ]]; then
+	rm -r "$path_tmp"
+fi
 
